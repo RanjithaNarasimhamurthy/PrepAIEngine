03143f06434e0b0f785a5ad64180ff8591773ef1
Ranjitha Narasimhamurthy <ranjitha.narasimhamurthy@Mac-416.lan>
2026-04-13 20:38:22 -0700

    Add render config, update CORS, commit processed data

--- backend/main.py
+++ backend/main.py
@@ -86,9 +86,15 @@ app = FastAPI(
     lifespan=lifespan,
 )
 
+_CORS_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    os.getenv("FRONTEND_URL", ""),        # set this in Render to your Vercel URL
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=[o for o in _CORS_ORIGINS if o],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
